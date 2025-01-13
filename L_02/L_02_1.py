d076f2ca8d63d610001a7aede61cb50fa075fe32
Viacheslav Titenko <marokovzp1@gmail.com>
2025-01-13 21:47:01 +0200

    Change code

--- L_02/L_02_1.py
+++ L_02/L_02_1.py
@@ -1,8 +1,8 @@
 user_input = input('Type 4 digits: ')
 print('You inputed next digits:')
 
-input = int(user_input)
-print (input // 1000)
-print ((input % 1000) // 100)
-print ((input % 100) // 10)
-print ((input % 10) // 1)
+digits = int(user_input)
+print (digits // 1000)
+print ((digits % 1000) // 100)
+print ((digits % 100) // 10)
+print ((digits % 10) // 1)
