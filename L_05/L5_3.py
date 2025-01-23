6982ae64f484fb01202cc65deb9c05a5e454a8bd
Viacheslav Titenko <marokovzp1@gmail.com>
2025-01-23 22:48:00 +0200

    Change code

--- L_05/L5_3.py
+++ L_05/L5_3.py
@@ -3,7 +3,9 @@ import string
 
 # my_str = 'Python Community'# -> #PythonCommunity
 # my_str = 'i like python community!'# -> #ILikePythonCommunity
-my_str = 'Should, I. subscribe? Yes!'# -> #ShouldISubscribeYes
+# my_str = 'Should, I. subscribe? Yes!'# -> #ShouldISubscribeYes
+my_str = input('type your name: ')
+
 
 print(my_str, end=' ')
 print('=>', end=' ')
@@ -12,19 +14,23 @@ my_str = my_str.title()
 lst = my_str.split()
 new_str ="".join(lst)
 
-lst = list(new_str)
+lst2 = list(new_str)
+print(lst2)
 
-for i, el in enumerate (lst):
+lst3 = []
+for i, el in enumerate (lst2):
     for el_punc in string.punctuation:
         if el == el_punc:
-            lst.remove(el)
+            lst3.append(i)
+for i in reversed(lst3):
+    lst2.pop(i)
 
-lst.insert(0, '#')
+lst2.insert(0, '#')
 
 my_len = 140
-if len(lst) > my_len:
-    lst = (lst[:my_len])
+if len(lst2) > my_len:
+    lst2 = (lst2[:my_len])
 
-new_str ="".join(lst)
+new_str ="".join(lst2)
 
 print(new_str)
