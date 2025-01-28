637166ef639abe334d06f4c0539ef89ea6e3a644
Viacheslav Titenko <marokovzp1@gmail.com>
2025-01-28 19:25:50 +0200

    Change code

---
+++ L_06/L6_1.py
@@ -0,0 +1,21 @@
+import string
+# print(string.ascii_letters)
+
+# my_str = "a-c"# -> abc
+# my_str = "a-a"# -> a
+# my_str = "s-H"# -> stuvwxyzABCDEFGH
+# my_str = "a-A"# -> abcdefghijklmnopqrstuvwxyzA
+my_str = input('type two letters with "-": ')
+
+print(my_str, end=' ')
+print('=>', end=' ')
+
+for i, el in enumerate(string.ascii_letters):
+    if el == my_str[0]:
+        start_i = i
+    if el == my_str[2]:
+        end_i = i
+
+for i, el in enumerate(string.ascii_letters):
+    if i >= start_i and i <= end_i:
+        print(el, end='')
