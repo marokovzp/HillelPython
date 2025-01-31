e239324c543d85155ab94554f10280686e215fcd
Viacheslav Titenko <marokovzp1@gmail.com>
2025-01-31 14:24:21 +0200

    Change code

--- L_07/L7_4.py
+++ L_07/L7_4.py
@@ -1,5 +1,8 @@
 def common_elements():
-		pass
-
+	num_set = set()
+	for i in range(0, 100):
+		if (i % 3) == 0 and (i % 5) == 0:
+			num_set.add(i)
+	return(num_set)
 
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
