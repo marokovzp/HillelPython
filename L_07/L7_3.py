ed7c2fd3b60209cd198e8b99dbbb062d41574b02
Viacheslav Titenko <marokovzp1@gmail.com>
2025-01-31 14:01:15 +0200

    Change code

---
+++ L_07/L7_3.py
@@ -0,0 +1,15 @@
+from win32verstamp import null_byte
+
+
+def second_index(text, some_str):
+  ind = text.find(some_str, text.find(some_str) + 1)
+
+  if ind >= 0: return (ind)
+  else: return
+
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
