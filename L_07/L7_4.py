670938372a2c8c8023492ef003d80b7dbc856dfb
Viacheslav Titenko <marokovzp1@gmail.com>
2025-01-31 15:04:49 +0200

    Change code

--- L_07/L7_4.py
+++ L_07/L7_4.py
@@ -1,8 +1,16 @@
 def common_elements():
-	num_set = set()
+	lst_1 = []
+	lst_2 = []
+
 	for i in range(0, 100):
-		if (i % 3) == 0 and (i % 5) == 0:
-			num_set.add(i)
-	return num_set
+		if (i % 3) == 0:
+			lst_1.append(i)
+		if (i % 5) == 0:
+			lst_2.append(i)
+
+	num_set_1 = set(lst_1)
+	num_set_2 = set(lst_2)
+	num_set_1.intersection_update(num_set_2)
+	return num_set_1
 
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
