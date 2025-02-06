a7fa9b73ea4c2ad4f31451afcd03da2a68f216ac
Viacheslav Titenko <marokovzp1@gmail.com>
2025-02-06 10:44:06 +0200

    Change code

--- L_08/L_08_gold.py
+++ L_08/L_08_gold.py
@@ -4,7 +4,7 @@ lst_3 = [9, 8, 2]
 lst_4 = [1, 3, 5, 6]
 lst_5 = [6, 2, 4, 4, 5]
 lst_6 = [9, 5, 3, 5, 5, 7]
-lst_7 = [7, 4, 6, 4, 6, 7, 8] #[7, 4, 6, 4, 7, 6, 8]
+lst_7 = [7, 4, 6, 4, 6, 6, 8] #[7, 4, 6, 4, 7, 6, 8] [7, 4, 6, 4, 6, 7, 8]
 lists = [lst_1, lst_2, lst_3, lst_4, lst_5, lst_6, lst_7]
 
 step = 0
@@ -18,10 +18,11 @@ for i, el in enumerate(lists):
     if lst[0] < lst[1]:
         step += 1
     elif lst[0] == lst[1]:
-        lst_next = lists[i + 1]
-        lst_next = lst_next[step:]
-        if lst_next[0] < lst_next[1]:
-            step += 1
+        if i < (len(lists) - 1):
+            lst_next = lists[i + 1]
+            lst_next = lst_next[step:]
+            if lst_next[0] < lst_next[1]:
+                step += 1
     my_summ += int(el[step])
     print(my_summ)
 print('Result', end=' ')
