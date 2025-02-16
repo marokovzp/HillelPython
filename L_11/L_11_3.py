06d50bf81aa2fc59e1b62a0d7a8fe2a98d8f7ed3
Viacheslav Titenko <marokovzp1@gmail.com>
2025-02-16 18:14:40 +0200

    Change code

--- L_11/L_11_3.py
+++ L_11/L_11_3.py
@@ -1,11 +1,10 @@
 def is_even(number):
     string = str(number)
     last = int(string[len(string)-1])
-    for i in (range(0, 10, 2)):
+    for i in range(0, 10, 2):
         if i == last:
             return True
     return False
-
 
 assert is_even(2494563894038**2) == True, 'Test1'
 assert is_even(1056897**2) == False, 'Test2'
